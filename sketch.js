d473fc2bf3de365672259d4430899f73e0520d63
arthursabrina <arthursabrina02@gmail.com>
2024-05-24 17:59:34 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,14 @@
 let drops = [];
-let palette;
-let bk;
-let newDrop = true;
-let currentColor;
+let palette, bk, currentColor, img, simg, wimg, himg;
 let w = window.innerWidth;
-let h = window.innerHeight;  
+let h = window.innerHeight;
 let counter = 0;
-let img;
-let thdrops;
-let simg;
-let wimg;
-let himg;
 let isPortrait;
 let showText = true;
 
 function preload() {
-  paletteP1 = [
+  // Preload images
+  this.paletteP1 = [
     loadImage("assets/us0.jpg"),
     loadImage("assets/us1.jpg"),
     loadImage("assets/us2.jpg"),
@@ -25,7 +18,7 @@ function preload() {
     loadImage("assets/us6.jpg")
   ];
 
-  paletteP2 = [
+  this.paletteP2 = [
     loadImage("assets/sa0.jpg"),
     loadImage("assets/sa1.jpg"),
     loadImage("assets/sa2.jpg"),
@@ -51,7 +44,7 @@ function setup() {
     color(255, 69, 0, 70)
   ];
 
-  palette2 = [
+  const palette2 = [
     color(25, 25, 112),
     color(65, 105, 225),
     color(72, 61, 139),
@@ -69,23 +62,21 @@ function setup() {
   drawingContext.shadowOffsetY = -5;
   drawingContext.shadowBlur = 15;
   drawingContext.shadowColor = color(50, 50, 50);
+
   bk = random(palette2);
   currentColor = random(palette);
 
-  // Set image source and load event
   isPortrait = h > w;
-  img = isPortrait ? random(paletteP2) : random(paletteP1);
+  img = isPortrait ? random(this.paletteP2) : random(this.paletteP1);
   calculateImageScale();
 }
 
 function calculateImageScale() {
   if (isPortrait) {
-    thdrops = 25;
     simg = w / img.width;
     wimg = 0;
     himg = (h / simg - img.height) / 2;
   } else {
-    thdrops = 50;
     simg = h / img.height;
     wimg = (w / simg - img.width) / 2;
     himg = 0;
@@ -94,10 +85,9 @@ function calculateImageScale() {
 
 function addInk(x, y, r, col) {
   let drop = new Drop(x, y, r, col);
-  for (let other of drops) {
-    other.marble(drop);
-  }
+  drops.forEach(other => other.marble(drop));
   drops.push(drop);
+  if (drops.length > (isPortrait ? 25 : 50)) drops.shift();
 }
 
 function mouseReleased() {
@@ -118,9 +108,8 @@ function touchStarted() {
 
 function draw() {
   background(bk);
-  
+
   if (img) {
-    // Draw image only if it's fully loaded
     drawingContext.save();
     drawingContext.scale(simg, simg);
     drawingContext.drawImage(img, wimg, himg);
@@ -130,22 +119,16 @@ function draw() {
       textAlign(CENTER, CENTER);
       textFont('Verdana', 36);
       fill(255);
-      text('Click on it!', w/2, 5*h/6);
+      text('Click on it!', w / 2, (5 * h) / 6);
     }
-  
-    if (drops.length >= thdrops) {
-      drops.shift();
-    }
-  
-    for (let drop of drops) {
-      drop.show();
-    }
-  
+
+    drops.forEach(drop => drop.show());
+
     if (mouseIsPressed) {
       currentColor = palette[counter % palette.length];
       counter++;
       addInk(mouseX, mouseY, 36, currentColor);
-  
+
       textSize(36);
       textAlign(CENTER);
       noStroke();
